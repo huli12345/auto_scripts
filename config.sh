878115643e8be4aa675f7a58c0c150a661739715
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-04-13 16:55:12 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,8 +7,8 @@ ln -s chip-certification-tool/backend/third_party/connectedhomeip/repo connected
 ln -s chip-certification-tool/cli cli
 sudo mv artifacts/dbus-fi.w1.wpa_supplicant1.service /etc/systemd/system/
 sudo mv artifacts/wpa_supplicant.conf /etc/wpa_supplicant/
-mv artifacts/start_otbr_setup.sh ~
-mv artifacts/start_test_harness.sh ~
+mv artifacts/start_otbr_setup.sh /home/ubuntu/
+mv artifacts/start_test_harness.sh /home/ubuntu/
 sudo chmod u+x start_otbr_setup.sh
 sudo chmod u+x start_test_harness.sh
 cd /home/ubuntu
