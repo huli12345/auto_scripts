e8b7960f7f3bb3373fd9b95f3a9eb407aba00d20
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-05-18 13:08:23 +0530

    Update auto_start.sh

--- auto_start.sh
+++ auto_start.sh
@@ -12,7 +12,7 @@ cd /home/ubuntu
 sudo bash ./kernal_patch.sh
 sudo bash ./dependencies.sh
 bash ./test_harness.sh &&
-bash ./connectedhomeip.sh &&
+#bash ./connectedhomeip.sh &&
 sudo bash ./config.sh
 touch flag.txt
 sudo reboot
