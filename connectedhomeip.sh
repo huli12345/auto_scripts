811acac4f069f0d3e60bc7c8f4c8755a61610ea7
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-05-18 13:08:04 +0530

    Update connectedhomeip.sh

--- connectedhomeip.sh
+++ connectedhomeip.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd /home/ubuntu
-git clone --recursive git@github.com:project-chip/connectedhomeip.git
-cd connectedhomeip/
-git checkout master
-cd /home/ubuntu
-mkdir apps
+#cd /home/ubuntu
+#git clone --recursive git@github.com:project-chip/connectedhomeip.git
+#cd connectedhomeip/
+#git checkout master
+#cd /home/ubuntu
+#mkdir apps
