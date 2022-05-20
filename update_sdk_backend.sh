b325bae1cebfaabd6f41e02520255ccf57977405
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-05-20 10:56:20 +0530

    added update for sdk in backend
    last 3 steps are manual.

--- update_sdk_backend.sh
+++ update_sdk_backend.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+cd ~
+cd chip-certification-tool/backend
+git checkout develop
+cd third_party/connectedhomeip/repo/
+git checkout master
+cd ../..
+./scripts/build_chip_tool.sh
+./scripts/build_simulated_apps.sh
+./scripts/build_managed_docker_images.sh
+echo " use docker exec -it <container name> /bin/bash "
+echo " execute : python3 ./scripts/build_tests_wrappers.py   ; in the container "  
+echo " after which restart the docker container using : docker-compose restart backend "
