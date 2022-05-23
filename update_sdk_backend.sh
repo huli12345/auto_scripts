89c6ddad2bae79f38e7b381e8d5811ebeba26ce9
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-05-23 15:28:00 +0530

    Update update_sdk_backend.sh

--- update_sdk_backend.sh
+++ update_sdk_backend.sh
@@ -5,10 +5,10 @@ cd chip-certification-tool/backend
 git checkout develop
 cd third_party/connectedhomeip/repo/
 git checkout master
-cd ../..
+cd ../../..
 ./scripts/build_chip_tool.sh
 ./scripts/build_simulated_apps.sh
 ./scripts/build_managed_docker_images.sh
-echo " use docker exec -it <container name> /bin/bash "
+echo " use : docker exec -it <container ID> /bin/bash "
 echo " execute : python3 ./scripts/build_tests_wrappers.py   ; in the container "  
 echo " after which restart the docker container using : docker-compose restart backend "
