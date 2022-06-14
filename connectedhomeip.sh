8d9a13bc9235a02fa716c42d2e1f6849edaa5c57
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-06-14 17:58:33 +0530

    Delete connectedhomeip.sh

--- connectedhomeip.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/ubuntu
-git clone --recursive git@github.com:project-chip/connectedhomeip.git
-cd connectedhomeip/
-git checkout master
-mv connectedhomeip/ chip-certification-tool/backend/third_party/connectedhomeip/repo
-echo "REPO MOVED"
