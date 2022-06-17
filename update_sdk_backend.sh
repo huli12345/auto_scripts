4e9b7a5cf663796d821fc3e26883ed025c7b3c6e
Srinivasan-78 <91538297+Srinivasan-78@users.noreply.github.com>
2022-06-17 15:33:26 +0530

    Update update_sdk_backend.sh

--- update_sdk_backend.sh
+++ update_sdk_backend.sh
@@ -15,6 +15,10 @@ cd ../../..
 #./scripts/build_chip_tool.sh
 #./scripts/build_simulated_apps.sh
 ./scripts/build_managed_docker_images.sh
-echo " use : docker exec -it <container ID> /bin/bash "
-echo " execute : python3 ./scripts/build_tests_wrappers.py   ; in the container "
-echo " after which restart the docker container using : docker restart <Container name> "
+sudo apt install figlet toilet -y
+figlet -f slant "Next Steps:"
+echo -e "\e[1;35m ......................................................................................... \e[0m"
+echo -e "\e[1;33m use : docker exec -it <container ID> /bin/bash \e[0m"
+echo -e "\e[1;32m execute : python3 ./scripts/build_tests_wrappers.py   ; in the container \e[0m"
+echo -e "\e[1;33m after which restart the docker container using : docker restart <Container name> \e[0m"
+echo -e "\e[1;35m ......................................................................................... \e[0m"
